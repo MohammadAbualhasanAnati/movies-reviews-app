45020dd6b19dcc75c0c0564c68c5863391f62dd1
Mohammad Anati (abualhasan) <abualhasan.anati@gmail.com>
2021-05-10 01:35:56 +0300

    Add MongoDB Connection

--- server.js
+++ server.js
@@ -2,16 +2,36 @@ const path = require('path')
 const express = require("express")
 const app = express()
 const env=require('dotenv')
+const mongoose=require('mongoose')
+mongoose.Promise = require('bluebird')
 
 env.config()
 
 const PORT=process.env.PORT
+const MONGO_CONNECTION=process.env.MONGO_CONNECTION
+var MONGO_OPTIONS = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // socketTimeoutMS: 0,
+    // keepAlive: true,
+    // reconnectTries: 30
+};
 
-
-const controllers=require('./Controllers')
-
-app.use('/api',controllers)
-
-app.listen(PORT,()=>{
-    console.log(`The app server started at ${PORT}`)
+mongoose.connect(MONGO_CONNECTION,MONGO_OPTIONS)
+const MONGOOSE_CONNECTION=mongoose.connection
+MONGOOSE_CONNECTION.on('error',console.error.bind(console,'MongoDB Connection Error!'))
+MONGOOSE_CONNECTION.once('connected',()=>{
+    console.log("MongoDB Connected success")
+    start_server()
 })
+
+const start_server=()=>{
+    const controllers=require('./Controllers')
+
+    app.use('/api',controllers)
+    
+    app.listen(PORT,()=>{
+        console.log(`The app server started at ${PORT}`)
+    })
+}
+
