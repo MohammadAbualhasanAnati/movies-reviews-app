ac7c8f97e0648bbfadbc4974a4aac61f3e0d92c8
Mohammad Anati (abualhasan) <abualhasan.anati@gmail.com>
2021-05-10 23:49:32 +0300

    Add simple urls in the /api directory to display added data in web browser as json

--- Controllers/index.js
+++ Controllers/index.js
@@ -3,6 +3,9 @@ const UserController=require('./UserController')
 const MovieController=require('./MovieController')
 const ReviewController=require('./ReviewController')
 
+routes.get('/',(req,res)=>{
+    res.status(200).send('<html><a href="api/movies/">Movies</a><br/><a href="api/reviews/">Reviews</a></html>').end()
+})
 routes.use('/users',UserController)
 routes.use('/movies',MovieController)
 routes.use('/reviews',ReviewController)
